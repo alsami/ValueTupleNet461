209c7c61f56840263493bc350b4611443d3ad705
al-khatib.sami <al-khatib.sami@fronius.com>
2020-05-10 10:57:05 +0200

    add

---
+++ ValueTuple/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace ValueTuple
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            (string a, string b) = ("a", "b");
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
